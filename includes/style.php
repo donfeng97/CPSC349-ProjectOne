019ddf4a2b150dd7a4cd50e3a6808e9c8c0467f7
Ying <ywen1306@csu.fullerton.edu>
2020-03-23 23:36:11 -0700

    Update style.php

--- includes/style.php
+++ includes/style.php
@@ -3,6 +3,4 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
     <link rel="stylesheet" href="stylesheets/stylesheet.css" />
-    <link rel="stylesheet" href="stylesheets/style_slides.css" />
-    <link rel="stylesheet" href="image-picker/image-picker.css"/> 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
