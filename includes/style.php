0650b1b1184695faa850de8b00da7f0998f37eef
Ying <ywen1306@csu.fullerton.edu>
2020-03-23 23:30:50 -0700

    Add files via upload

---
+++ includes/style.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="stylesheets/stylesheet.css" />
+    <link rel="stylesheet" href="stylesheets/style_slides.css" />
+    <link rel="stylesheet" href="image-picker/image-picker.css"/> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
---
+++ register2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'includes/script.php'?>
+    <?php include 'includes/style.php'?>
+</head>
+
+<body>
+<div class="logo"><a href="#"> <img src="img/logo31.png"></a> 
+
+		<nav>
+            <ul>
+                <li> <a id="btnSignin" href="log" class="btn btn-danger navbar-btn" style="float: right; margin-top: 30px; margin-right: 30px;">Sign
+                        In</a></li>
+            </ul>
+
+        </nav>
+    </div>
+    <div>
+
+    </div>
+	</body>
+</html>
