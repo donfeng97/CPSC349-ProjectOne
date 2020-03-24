7a092f67d57b765233392e232890f5e0558d332d
ICE <donfeng97@csu.fullerton.edu>
2020-03-24 00:57:14 -0700

    yea

--- home_feed.php
+++ home_feed.php
@@ -1,6 +1,50 @@
+<?php
+require_once "config.php";
+
+$message = $userID = "";
+$school = "csuf";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(!empty(trim($_POST["post"]))) {
+        $message = trim($_POST["post"]);
+    }
+
+    //if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+        $userID = 5;
+    //} else {
+        //header("location: login.php");
+    //}
+
+    if (!empty($message)) {
+        $sql = "INSERT INTO posts (message, school, userID) VALUES (?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "sss", $param_message, $param_school, $param_userID);
+            
+            // Set parameters
+            $param_message = $message;
+            $param_school = $school;
+            $param_userID = $userID;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Refresh current page
+                header("Refresh:0");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+        mysqli_close($link);
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -12,7 +56,6 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <title>Home Feed!</title>
 </head>
-
 <body>
     <div>
         <div id="page-wrapper">
@@ -35,12 +78,13 @@
             <div style="display: block;">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <form data-my-form="form">
+                        <form data-my-form="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                             <div class="form-group">
                                 <label for="studentForm">Post</label>
                                 <input class="form-control" name="post" id="pp" required>
                             </div>
                             <input id="btnSignin" type="submit" class="btn btn-primary" value="Post"></input>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -61,6 +105,6 @@
             </ul>
             <!-- Display posted data -->
         </div>
+    </div>
 </body>
-
 </html>
